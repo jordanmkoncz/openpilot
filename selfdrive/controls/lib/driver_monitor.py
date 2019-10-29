adde98d1d0461c7193bc6163b464e3cf86b55c46
Jordan Koncz <jordanmkoncz@gmail.com>
2019-10-30 10:27:12 +1100

    Increase awareness time limit and maximum terminal alerts.

--- selfdrive/controls/lib/driver_monitor.py
+++ selfdrive/controls/lib/driver_monitor.py
@@ -4,7 +4,7 @@ from selfdrive.controls.lib.drive_helpers import create_event, EventTypes as ET
 from common.filter_simple import FirstOrderFilter
 from common.stat_live import RunningStatFilter
 
-_AWARENESS_TIME = 1800.  # 30 minute limit without user touching steering wheels make the car enter a terminal status
+_AWARENESS_TIME = 3600.  # 60 minute limit without user touching steering wheels make the car enter a terminal status
 _AWARENESS_PRE_TIME_TILL_TERMINAL = 10.  # a first alert is issued 10s before expiration
 _AWARENESS_PROMPT_TIME_TILL_TERMINAL = 5.  # a second alert is issued 5s before start decelerating the car
 _DISTRACTED_TIME = 11.
@@ -29,7 +29,7 @@ _POSE_OFFSET_MAX_COUNT = 3600 # stop deweighting new data after 6 min, aka "shor
 _RECOVERY_FACTOR_MAX = 5. # relative to minus step change
 _RECOVERY_FACTOR_MIN = 1.25 # relative to minus step change
 
-MAX_TERMINAL_ALERTS = 3      # not allowed to engage after 3 terminal alerts
+MAX_TERMINAL_ALERTS = 20      # not allowed to engage after 20 terminal alerts
 
 # model output refers to center of cropped image, so need to apply the x displacement offset
 RESIZED_FOCAL = 320.0
